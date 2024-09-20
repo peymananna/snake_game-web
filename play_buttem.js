e88398f76718f2c9183a42dc3002be8b027b6eac
Peyman <peyman@Peymans-MacBook-Pro.local>
2024-09-20 12:34:37 +0330

    Initial commit - added Snake Game files

---
+++ play_buttem.js
@@ -0,0 +1,4 @@
+document.getElementById('play-button').addEventListener('click', () => {
+    // وقتی دکمه پلی زده می‌شود، درخواست به سرور ارسال می‌شود یا بازی اجرا می‌شود
+    alert('Game is starting!'); // در اینجا بازی را اجرا کن یا به صفحه بازی هدایت کن
+});
